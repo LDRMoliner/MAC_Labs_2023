bd1bb29c3cb9ba5e1ba8c0e76d7b30c4eb09527d
MolinerBR <molinamikel0404@gmail.com>
2023-10-07 11:49:57 +0200

    Hecho el laboratorio 4

--- Lab04/Code For Students/Reduction_3problems/Lab04_reduction_3problems.py
+++ Lab04/Code For Students/Reduction_3problems/Lab04_reduction_3problems.py
@@ -1,10 +1,25 @@
 from vertex_cover import solve_vc
 
+def get_complementary_graph(graph):
+    return [[1 if elem == 0 and not row_index == index else 0 for index, elem in enumerate(row)] for row_index, row in enumerate(graph)]
+
+def get_independent_set (graph):
+    sol = solve_vc(graph)
+    return [1 if num == 0 else 0 for num in sol]
+def get_clique(graph):
+    comp_graph = get_complementary_graph(graph)
+    return get_independent_set(comp_graph)
 
 
-def multisolve(graph, problem):    
+def multisolve(graph, problem):
     #TODO
-    
+    if (problem == "VERTEX COVER"):
+        return solve_vc(graph)
+    if (problem == "CLIQUE"):
+        return get_clique(graph)
+    if (problem == "INDEPENDENT SET"):
+        return get_independent_set(graph)
+    return 0
 
 def test():
    graph = [[0, 0, 1, 0], 
@@ -38,7 +53,7 @@ def test():
             [1,0,1,0,1,0,0],
             [1,0,0,1,0,1,0],
             [1,0,0,0,1,0,1],
-            [1,1,0,0,0,0,1]]   
+            [1,1,0,0,0,0,1]]
    
    sol_vertex = multisolve(graph, "VERTEX COVER")
    sol_clique = multisolve(graph, "CLIQUE")
