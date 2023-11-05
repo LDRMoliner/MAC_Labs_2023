33a3f6b962f67fd91981d93d3cb8df713ee97e78
MolinerBR <molinamikel0404@gmail.com>
2023-11-05 15:57:12 +0100

    Hecho vertex cover 2.

--- lab06/Code For students/lab06_vertex_cover_2.py
+++ lab06/Code For students/lab06_vertex_cover_2.py
@@ -1,5 +1,6 @@
 from time import time
 
+
 # cover es una lista de 0s, 1s y Nones.
 # Si i es la posicion de un 1, el i-esimo nodo esta en el cover.
 # Si i es la posicion de un 0, el i-esimo nodo no esta en el cover.
@@ -9,21 +10,22 @@ from time import time
 # si los nodos del cover pueden llegar a cubirir todas las aristas del grafo o no.
 def partial_validity_check(cover, graph):
     # TODO: Programa el codigo de la funcion
-    
+    for i in range(len(graph)):
+        for j in range(len(graph)):
+            if graph[i][j] == 1 and cover[i] == 0 and cover[j] == 0:
+                return False
+    return True
 
 
 # vertex_cover_tree inicializa y llama al arbol de busqueda
 def vertex_cover_tree(graph):
     n = len(graph)
-    cover = [None]*n
+    cover = [None] * n
     return recursive_vertex_cover(graph, cover)
 
 
-
-
-# recursive_vertex_cover construye recursivamente el arbol de busqueda 
+# recursive_vertex_cover construye recursivamente el arbol de busqueda
 def recursive_vertex_cover(graph, cover):
-
     ############
     # TODO: Programa esta parte de la funcion
     #
@@ -32,51 +34,52 @@ def recursive_vertex_cover(graph, cover):
     # En otro caso, 
     # si el cover esta completo, lo devuelve.
     # Si esta incompleto, encuentra un nodo v que no esta en el cover.
-    
-    
+
+    if (not partial_validity_check(cover, graph)):
+        return [1] * len(cover)
+    if (None not in cover):
+        return cover
+    v = cover.index(None)
+
     # Final de tu codigo
     # Lo siguiente abre las dos ramas del arbol de busqueda.
     # No modificar nada.
     ##############
-        copy_cover = list(cover)
-        cover[v] = 0
-        c0 = recursive_vertex_cover(graph, cover)
-        cover = list(copy_cover)
-        cover[v] = 1
-        c1 = recursive_vertex_cover(graph, cover)
-        return c0 if c0.count(1) < c1.count(1) else c1
+    copy_cover = list(cover)
+    cover[v] = 0
+    c0 = recursive_vertex_cover(graph, cover)
+    cover = list(copy_cover)
+    cover[v] = 1
+    c1 = recursive_vertex_cover(graph, cover)
+    return c0 if c0.count(1) < c1.count(1) else c1
 
 
 def test():
-    
-    g1 =  [[0, 1],
-           [1, 0]]
-       
+    g1 = [[0, 1],
+          [1, 0]]
+
     g2 = [[0, 1, 1],
           [1, 0, 0],
           [1, 0, 0]]
-        
-    
+
     g3 = [[0, 1, 1, 1, 1],
           [1, 0, 0, 0, 1],
           [1, 0, 0, 1, 1],
           [1, 0, 1, 0, 1],
           [1, 1, 1, 1, 0]]
-    
-        
+
     g4 = [[0, 1, 1, 1],
           [1, 0, 1, 0],
           [1, 1, 0, 1],
           [1, 0, 1, 0]]
-    
-       
+
     g5 = [[0, 1, 1, 0, 0, 0],
           [1, 0, 0, 1, 1, 0],
           [1, 0, 0, 1, 1, 1],
           [0, 1, 1, 0, 0, 1],
           [0, 1, 1, 0, 0, 0],
           [0, 0, 1, 1, 0, 0]]
-    
+
     g6 = [[0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0],
           [1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
           [1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0],
@@ -92,7 +95,7 @@ def test():
           [0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1],
           [0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1],
           [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0]]
-    
+
     g7 = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
@@ -113,67 +116,65 @@ def test():
           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]]
 
-   
- # #    Descomentar para probar la funcion partial_validity_check
- #     assert not partial_validity_check([0,0], g1)
- #     assert not partial_validity_check([0,0,1], g2)
- #     assert partial_validity_check([1,None,None], g2) 
- #     assert partial_validity_check([0,None,None], g2) 
- #     assert partial_validity_check([1,0,0], g2) 
- #     assert partial_validity_check([1,1,0], g2) 
- #     assert partial_validity_check([0,1,None], g2)
- #     assert not partial_validity_check([0,None,0], g2) 
- #     assert not partial_validity_check([0, 1, 1, 0, 1, 0], g5)
- #     assert partial_validity_check([0, 1, 1, 1, 0, 0], g5) 
- #     assert partial_validity_check([1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1], g6)
- # ##################################################################  
-     
-  
- # ##Descomentar para probar la funcion recursive_vertex_cover
- #     assert vertex_cover_tree(g1) in [[1,0],[0,1]]
- #     assert vertex_cover_tree(g2)  == [1,0,0]
- #     assert vertex_cover_tree(g3) in [[1, 0, 1, 0, 1], 
- #                                       [1, 0, 0, 1, 1]]
- #     assert vertex_cover_tree(g4)  == [1, 0, 1, 0]
- #     assert vertex_cover_tree(g5)  in  [[0, 1, 1, 1, 0, 0], 
- #                                         [0, 1, 1, 0, 0, 1]]
-     
- #     assert vertex_cover_tree(g6) in [[1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1],
- #                                       [1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1],
- #                                       [1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
- #                                       [1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1],
- #                                       [1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1],
- #                                       [1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1],
- #                                       [1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0],
- #                                       [1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1],
- #                                       [1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0],
- #                                       [1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1],
- #                                       [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1],
- #                                       [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1],
- #                                       [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0],
- #                                       [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1],
- #                                       [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0],
- #                                       [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
- #                                       [1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1],
- #                                       [1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0],
- #                                       [1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1],
- #                                       [1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1],
- #                                       [1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1],
- #                                       [1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0],
- #                                       [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1],
- #                                       [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0],
- #                                       [1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
- #                                       [1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1],
- #                                       [1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0],
- #                                       [1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1],
- #                                       [1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1],
- #                                       [1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1],
- #                                       [1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1]]
+    # #    Descomentar para probar la funcion partial_validity_check
+    assert not partial_validity_check([0, 0], g1)
+    assert not partial_validity_check([0, 0, 1], g2)
+    assert partial_validity_check([1, None, None], g2)
+    assert partial_validity_check([0, None, None], g2)
+    assert partial_validity_check([1, 0, 0], g2)
+    assert partial_validity_check([1, 1, 0], g2)
+    assert partial_validity_check([0, 1, None], g2)
+    assert not partial_validity_check([0, None, 0], g2)
+    assert not partial_validity_check([0, 1, 1, 0, 1, 0], g5)
+    assert partial_validity_check([0, 1, 1, 1, 0, 0], g5)
+    assert partial_validity_check([1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1], g6)
+    # ##################################################################
 
- #     assert vertex_cover_tree(g7) in [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]]    
+    # ##Descomentar para probar la funcion recursive_vertex_cover
+    assert vertex_cover_tree(g1) in [[1, 0], [0, 1]]
+    assert vertex_cover_tree(g2) == [1, 0, 0]
+    assert vertex_cover_tree(g3) in [[1, 0, 1, 0, 1],
+                                     [1, 0, 0, 1, 1]]
+    assert vertex_cover_tree(g4) == [1, 0, 1, 0]
+    assert vertex_cover_tree(g5) in [[0, 1, 1, 1, 0, 0],
+                                     [0, 1, 1, 0, 0, 1]]
+
+    assert vertex_cover_tree(g6) in [[1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1],
+                                     [1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1],
+                                     [1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+                                     [1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+                                     [1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1],
+                                     [1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1],
+                                     [1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0],
+                                     [1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+                                     [1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0],
+                                     [1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1],
+                                     [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1],
+                                     [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1],
+                                     [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0],
+                                     [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1],
+                                     [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0],
+                                     [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+                                     [1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+                                     [1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0],
+                                     [1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1],
+                                     [1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1],
+                                     [1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1],
+                                     [1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0],
+                                     [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1],
+                                     [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0],
+                                     [1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+                                     [1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1],
+                                     [1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0],
+                                     [1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1],
+                                     [1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1],
+                                     [1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1],
+                                     [1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1]]
+
+    assert vertex_cover_tree(g7) in [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]]
 
 
- start_time = time()
- test()
- elapsed_time = time() - start_time   
- print("Elapsed time: %0.10f seconds." % elapsed_time)           
+start_time = time()
+test()
+elapsed_time = time() - start_time
+print("Elapsed time: %0.10f seconds." % elapsed_time)
--- lab06/Code For students/lab06_vertex_cover_3.py
+++ lab06/Code For students/lab06_vertex_cover_3.py
@@ -1,5 +1,5 @@
 from time import time
-
+from lab06_vertex_cover_2 import partial_validity_check
 ## vertex_cover_tree inicializa y llama al arbol de busqueda
 def vertex_cover_tree(graph):
     n = len(graph)
@@ -17,30 +17,52 @@ def recursive_vertex_cover(graph, cover):
     # Si no los hay, completa el cover decidiendo si los que faltan deben formar parte 
     # del cover o no y una vez hecho esto, devuelve el cover completo.
     # En otro caso continua con u y v
-    
+    if(not partial_validity_check(cover, graph)):
+        return [1]*len(cover)
+    if None not in cover:
+        return cover
+    found_pair = False
+    u = None
+    v = None
+    for i in range(len(graph)):
+        for j in range(len(graph)):
+            if graph[i][j] == 1 and cover[i] == None and cover[j] == None:
+                u = i
+                v = j
+                found_pair = True
+                break
+        if found_pair:
+            break
+    if not found_pair:
+        for i in range(len(graph)):
+            for j in range(len(graph)):
+                if graph[i][j] == 1 and cover[i] == 1 and cover[j] == None:
+                    cover[j] = 0
+        return cover
+
     
     # Final de tu codigo
     # Lo siguiente abre las tres ramas del arbol de busqueda.
     # No modificar nada.
     ##############
-        copy_cover = list(cover)
-        cover[u] = 1
-        cover[v] = 0
-        c10 = recursive_vertex_cover(graph, cover)
-        cover = list(copy_cover)
-        cover[u] = 0
-        cover[v] = 1
-        c01 = recursive_vertex_cover(graph, cover)
-        cover = list(copy_cover)
-        cover[u] = 1
-        cover[v] = 1
-        c11 = recursive_vertex_cover(graph, cover)
-        if c10.count(1) <= min(c01.count(1), c11.count(1)):
-            return c10
-        elif c01.count(1) <= c11.count(1):
-            return c01
-        else:
-            return c11
+    copy_cover = list(cover)
+    cover[u] = 1
+    cover[v] = 0
+    c10 = recursive_vertex_cover(graph, cover)
+    cover = list(copy_cover)
+    cover[u] = 0
+    cover[v] = 1
+    c01 = recursive_vertex_cover(graph, cover)
+    cover = list(copy_cover)
+    cover[u] = 1
+    cover[v] = 1
+    c11 = recursive_vertex_cover(graph, cover)
+    if c10.count(1) <= min(c01.count(1), c11.count(1)):
+        return c10
+    elif c01.count(1) <= c11.count(1):
+        return c01
+    else:
+        return c11
     
 def test():
     
@@ -108,65 +130,66 @@ def test():
           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]]
 
+
    
     
 # #    Descomentar para probar la funcion partial_validity_check
-#     assert not partial_validity_check([0,0], g1)
-#     assert not partial_validity_check([0,0,1], g2)
-#     assert partial_validity_check([1,None,None], g2) 
-#     assert partial_validity_check([0,None,None], g2) 
-#     assert partial_validity_check([1,0,0], g2) 
-#     assert partial_validity_check([1,1,0], g2) 
-#     assert partial_validity_check([0,1,None], g2)
-#     assert not partial_validity_check([0,None,0], g2) 
-#     assert not partial_validity_check([0, 1, 1, 0, 1, 0], g5)
-#     assert partial_validity_check([0, 1, 1, 1, 0, 0], g5) 
-#     assert partial_validity_check([1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1], g6)
+    assert not partial_validity_check([0,0], g1)
+    assert not partial_validity_check([0,0,1], g2)
+    assert partial_validity_check([1,None,None], g2)
+    assert partial_validity_check([0,None,None], g2)
+    assert partial_validity_check([1,0,0], g2)
+    assert partial_validity_check([1,1,0], g2)
+    assert partial_validity_check([0,1,None], g2)
+    assert not partial_validity_check([0,None,0], g2)
+    assert not partial_validity_check([0, 1, 1, 0, 1, 0], g5)
+    assert partial_validity_check([0, 1, 1, 1, 0, 0], g5)
+    assert partial_validity_check([1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1], g6)
 # ##################################################################  
     
  
 # ##Descomentar para probar la funcion recursive_vertex_cover
-#     assert vertex_cover_tree(g1) in [[1,0],[0,1]]
-#     assert vertex_cover_tree(g2)  == [1,0,0]
-#     assert vertex_cover_tree(g3) in [[1, 0, 1, 0, 1], 
-#                                       [1, 0, 0, 1, 1]]
-#     assert vertex_cover_tree(g4)  == [1, 0, 1, 0]
-#     assert vertex_cover_tree(g5)  in  [[0, 1, 1, 1, 0, 0], 
-#                                         [0, 1, 1, 0, 0, 1]]
+    assert vertex_cover_tree(g1) in [[1,0],[0,1]]
+    assert vertex_cover_tree(g2)  == [1,0,0]
+    assert vertex_cover_tree(g3) in [[1, 0, 1, 0, 1],
+                                       [1, 0, 0, 1, 1]]
+    assert vertex_cover_tree(g4)  == [1, 0, 1, 0]
+    assert vertex_cover_tree(g5)  in  [[0, 1, 1, 1, 0, 0],
+                                         [0, 1, 1, 0, 0, 1]]
     
-#     assert vertex_cover_tree(g6) in [[1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1],
-#                                       [1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1],
-#                                       [1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
-#                                       [1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1],
-#                                       [1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1],
-#                                       [1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1],
-#                                       [1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0],
-#                                       [1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1],
-#                                       [1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0],
-#                                       [1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1],
-#                                       [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1],
-#                                       [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1],
-#                                       [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0],
-#                                       [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1],
-#                                       [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0],
-#                                       [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
-#                                       [1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1],
-#                                       [1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0],
-#                                       [1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1],
-#                                       [1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1],
-#                                       [1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1],
-#                                       [1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0],
-#                                       [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1],
-#                                       [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0],
-#                                       [1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
-#                                       [1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1],
-#                                       [1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0],
-#                                       [1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1],
-#                                       [1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1],
-#                                       [1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1],
-#                                       [1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1]]
+    assert vertex_cover_tree(g6) in [[1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1],
+                                       [1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1],
+                                       [1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+                                       [1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+                                       [1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1],
+                                       [1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1],
+                                       [1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0],
+                                       [1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+                                       [1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0],
+                                       [1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1],
+                                       [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1],
+                                       [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1],
+                                       [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0],
+                                       [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1],
+                                       [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0],
+                                       [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+                                       [1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+                                       [1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0],
+                                       [1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1],
+                                       [1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1],
+                                       [1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1],
+                                       [1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0],
+                                       [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1],
+                                       [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0],
+                                       [1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+                                       [1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1],
+                                       [1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0],
+                                       [1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1],
+                                       [1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1],
+                                       [1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1],
+                                       [1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1]]
 
-#     assert vertex_cover_tree(g7) in [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]]    
+    assert vertex_cover_tree(g7) in [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]]
 
 
 start_time = time()
